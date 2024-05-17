c6297dd350f127f16ec86a973ca36a6420daa56c
OtiliaNicola <julio.guerrero@ezentis.com>
2024-05-17 11:06:49 +0200

    app

--- src/componets/App.jsx
+++ src/componets/App.jsx
@@ -1,12 +1,15 @@
 import "../styles/App.scss";
 import Header from "./Header";
 import Board from "./Board";
+//import Form from "./Form";
+import { useState } from "react";
 
 
 function App() {
   
   const [groguPosition, setGroguPosition] = useState(0);
   const [merchandiseList, setMerchandiseList] = useState(['Cookie', 'Egg', 'Frog']);
+  //const [name, setName] = useState("");
 
   const rollDice = () => {
     const numberRandom = getRandomNumber(1,4);
@@ -15,9 +18,13 @@ function App() {
     setGroguPosition(groguPosition + 1);
     message = 'Grogu ha avanzado una casilla.';
   } else (numberRandom === 1){
-    setMerchandiseList(po)
+    const removedMerchandise = merchandiseList[namberRandom - 1];
+    const updatedMerchandiseList = merchandiseList.filter((item, index) => index !== numberRandom - 1);
+    setMerchandiseList(updatedMerchandiseList);
+    message = `Se ha descargado una mercancía: ${removedMerchandise}.`;
   }
-
+    const getRandomNumber = (min, max) => {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
 }}
 
   return (
@@ -25,25 +32,15 @@ function App() {
     <Header/>
     <main className="page">
       <Board/>
+     {/* <Form name={name}/>*/}
       <section>
-        <button className="dice">Lanzar Dado</button>
+        <button className="dice" onClick={rollDice}>Lanzar Dado</button>
         <div className="game-status">En curso</div>
       </section>
 
       <section className="goods-container">
-        <div className="goods-item">🍪</div>
-        <div className="goods-item">🍪</div>
-        <div className="goods-item">🍪</div>
-      </section>
-      <section className="goods-container">
-        <div className="goods-item">🥚</div>
-        <div className="goods-item">🥚</div>
-        <div className="goods-item">🥚</div>
-      </section>
-      <section className="goods-container">
-        <div className="goods-item">🐸</div>
-        <div className="goods-item">🐸</div>
-        <div className="goods-item">🐸</div>
+      {merchandiseList.map((item, index) => (
+            <div className="goods-item" key={index}>{item}</div>))}
       </section>
       <section>
         <button className="restart-button">Reiniciar Juego</button>
---
+++ src/componets/Form.jsx
@@ -0,0 +1,16 @@
+
+
+/*function Form(name) {
+
+    const handleChange (ev) =>{
+        ev.target.value
+    }
+
+  return (
+    <div>
+        <form action="" class="form"></form>
+    </div>
+  )
+}
+
+export default Form*/
